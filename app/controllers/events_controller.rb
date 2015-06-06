dce5a5a230cca5406703fd6903fa40b0e9054611
Mitchell Anicas <thisismitch@gmail.com>
2015-06-05 23:57:21 -0400

    added CRUD for events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,26 +1,46 @@
 class EventsController < ApplicationController
+  before_action :set_event, only: [:edit, :show, :update, :destroy]
+
   def index
   	@events = Event.all
   end
 
   def show
-  	# view event (list questions) - add event ID parameter
-  	
-  	@event = Event.find(params[:id])  # replace with next line
-  	@questions = @event.questions
-
-  	
-  	### Test stuff
-  	# @question = @event.questions.first
-  	# @username = @question.user.name
+   	@questions = @event.questions
   end
 
   def new
+    @event = Event.new
+  end
+
+  def create
+    @user = current_user.id
+    
+    @event = Event.create(event_params)
+    # redirect_to event_path(@event)
+    redirect_to @event, notice: 'Event was created.'
+  end
+
+  def edit
+  end
+
+  def update
+    @event.update(event_params)
+    redirect_to @event, notice: 'Event was updated.'
+  end
+
+  def destroy
+    @event.destroy
+    redirect_to events_path, notice: 'Event was deleted.'
   end
 
   private
 
-  def render_question(id)
+  def event_params
+    params.require(:event).permit(:name, :description)
+  end
 
+  def set_event
+    @event = Event.find(params[:id])
   end
 end
