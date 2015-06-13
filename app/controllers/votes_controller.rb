1ccbffdf634c2190d6794af9b6b0b656d6b1a3b9
Mitchell Anicas <thisismitch@gmail.com>
2015-06-13 08:49:43 -0400

    added voting and vote switching

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,21 +1,34 @@
 class VotesController < ApplicationController
-  before_action :set_question
+  before_action :set_question_and_vote
 
   def upvote
-    # Need to implement updates
-    @vote = @question.votes.create(user_id: current_user.id, type_of: 'up')
-    redirect_to :back, notice: 'Question upvoted.'
+    if @vote.nil?
+      @vote = @question.votes.create(user_id: current_user.id, type_of: 'up')
+      redirect_to :back, notice: 'Question upvoted.'
+    elsif @vote.type_of == 'down'
+      @vote.update(type_of: 'up')
+      redirect_to :back, notice: 'Question upvoted.'
+    end
   end
 
   def downvote
-    # Need to implement updates
-    @vote = @question.votes.create(user_id: current_user.id, type_of: 'down')
-    redirect_to :back, notice: 'Question downvoted.'
+    if @vote.nil?
+      @vote = @question.votes.create(user_id: current_user.id, type_of: 'down')
+      redirect_to :back, notice: 'Question downvoted.'
+    elsif @vote.type_of == 'up'
+      @vote.update(type_of: 'down')
+      redirect_to :back, notice: 'Question downvoted.'
+    end
+  end
+
+  def retract_vote
+
   end
 
   private
 
-  def set_question
+  def set_question_and_vote
     @question = Question.find(params[:id])
+    @vote = @question.votes.find_by(user_id: current_user.id)
   end
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,6 @@
 class Vote < ActiveRecord::Base
+	validates_uniqueness_of :user_id, scope: :question_id
+  
   belongs_to :user
   belongs_to :question
 end
