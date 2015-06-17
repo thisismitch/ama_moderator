25576ccaa7b9d89cf91ab45b675df7a653383e4d
Mitchell Anicas <thisismitch@gmail.com>
2015-06-16 21:24:05 -0400

    improve devise views and delete non-oauth routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }, :skip => [:registrations, :passwords]
 
   concern :has_votes do
   	resources :votes, only: :create
