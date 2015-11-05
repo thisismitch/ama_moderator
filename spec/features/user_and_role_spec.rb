5f1da3e6f25148414ec0ecd7a5d78fb30d4bbd1f
Mitchell Anicas <thisismitch@gmail.com>
2015-11-04 20:56:19 -0500

    add basic integration tests

--- spec/features/user_and_role_spec.rb
+++ spec/features/user_and_role_spec.rb
@@ -2,10 +2,29 @@ require 'rails_helper'
 
 
 describe 'with users and roles' do
+  let(:normal_user) { FactoryGirl.create(:normal_user) }
+  let(:admin_user) { FactoryGirl.create(:admin_user) }
+
+  it 'shows sign-in page to non-logged-in user' do
+    visit root_path
+    expect(current_path).to eq(new_user_session_path)
+  end
+
   it 'allows a logged-in user to view the event index page' do
-    user = FactoryGirl.create(:normal_user)
-    login_as(user, :scope => :user)
+    login(normal_user)
     visit events_path
     expect(current_path).to eq(events_path)
+  end
+
+  it 'allows an admin user to view the users page' do
+    login(admin_user)
+    visit users_path
+    expect(current_path).to eq(users_path)
+  end
+
+  it 'does not allow a normal user to view the users page' do
+    login(normal_user)
+    visit users_path
+    expect(current_path).to_not eq(users_path)
   end
 end
--- spec/support/devise.rb
+++ spec/support/devise.rb
@@ -1,2 +1,12 @@
 include Warden::Test::Helpers
 Warden.test_mode!
+
+RSpec.configure do |config|
+  config.after :each do
+    Warden.test_reset!
+  end
+end
+
+def login(user)
+ login_as(user, :scope => :user)
+end
