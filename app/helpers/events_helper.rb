cc9037fdd93674cddb5714733c47ba1573009ce4
Mitchell Anicas <thisismitch@gmail.com>
2015-11-05 19:56:54 -0500

    format date and time on events index

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,7 @@
 module EventsHelper
+  def format_datetime(event_datetime)
+    unless event_datetime.nil?
+      event_datetime.strftime("%B %d, %Y at%l:%M%p")
+    end
+  end
 end
--- spec/features/add_event_spec.rb
+++ spec/features/add_event_spec.rb
@@ -1,21 +1,24 @@
 require 'rails_helper'
 require 'helpers/select_datetime_helper'
 
+
 describe "adding events" do
   include SelectDatetime
+  include EventsHelper
 
   let(:admin_user) { FactoryGirl.create(:admin_user) }
+  let(:datetime) { DateTime.new(2015, 10, 31, 18, 30) }
 
-  it "allows a user to create an event with name, datetime, description" do
+  it "allows an admin user to create an event with name, datetime, description" do
     login(admin_user)
     visit new_event_path
     fill_in "Name", with: "AMA 10.31"
     fill_in "Description", with: "Ask me anything about Halloween"
-    select_date_and_time DateTime.new(2015, 10, 31, 18, 30), from: "event_scheduled_datetime"
+    select_date_and_time datetime, from: "event_scheduled_datetime"
     click_button("Create Event")
     visit events_path
     expect(page).to have_content("AMA 10.31")
     expect(page).to have_content("Halloween")
-    expect(page).to have_content("2015-10-31")
+    expect(page).to have_content(format_datetime(datetime))
   end
 end
