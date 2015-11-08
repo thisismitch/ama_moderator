9e6e63df46e58080c76ff73acedbe7ca8607e5b6
Mitchell Anicas <thisismitch@gmail.com>
2015-11-08 11:08:57 -0500

    add relative time to event helper

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,7 +1,13 @@
 module EventsHelper
   def format_datetime(event_datetime)
     unless event_datetime.nil?
-      event_datetime.strftime('%B %d, %Y at%l:%M%p')
+      event_datetime.strftime('%B %d, %Y at %l:%M%p')
+    end
+  end
+
+  def relative_time_to_event(event)
+    if !event.closed? && !event.scheduled_datetime.nil?
+      distance_of_time_in_words(DateTime.now, event.scheduled_datetime)
     end
   end
 
