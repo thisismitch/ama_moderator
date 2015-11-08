6ac625ae0116247004bcdff1aaa9e37fd1a88327
Mitchell Anicas <thisismitch@gmail.com>
2015-11-08 10:10:13 -0500

    sort events in descending order by scheduled_datetime, then created_at

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,7 +1,12 @@
 module EventsHelper
   def format_datetime(event_datetime)
     unless event_datetime.nil?
-      event_datetime.strftime("%B %d, %Y at%l:%M%p")
+      event_datetime.strftime('%B %d, %Y at%l:%M%p')
     end
   end
+
+  def descending_events(events)
+    events.order(scheduled_datetime: :desc, created_at: :desc)
+  end
+
 end
