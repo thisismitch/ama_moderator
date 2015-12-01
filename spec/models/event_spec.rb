0772bec92a2fb315b2b646412de52730012c0c4d
Mitchell Anicas <thisismitch@gmail.com>
2015-12-01 17:46:55 -0500

    use be_closed matcher in event specs

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -3,32 +3,37 @@ require 'rails_helper'
 RSpec.describe Event do
   describe "manual status" do
     let(:event) { Event.new }
+
     it "considers a new event to be open" do
-      expect(event.closed?).to be_falsy
+      expect(event).not_to be_closed
     end
 
     it "can be closed" do
       event.close
-      expect(event.closed?).to be_truthy
+
+      expect(event).to be_closed
     end
 
     it "can be re-opened" do
       event.close
       event.open
-      expect(event.closed?).to be false
+
+      expect(event).not_to be_closed
     end
   end
 
   describe 'status' do
     let(:event) { Event.new }
+
     it 'past events are closed' do
       event.scheduled_datetime = DateTime.now - 1
-      expect(event.closed?).to be true
+
+      expect(event).to be_closed
     end
 
     it 'future events are open' do
       event.scheduled_datetime = DateTime.tomorrow
-      expect(event.closed?).to be false
+      expect(event).not_to be_closed
     end
   end
 
@@ -38,7 +43,7 @@ RSpec.describe Event do
 
     describe '#vote_count' do
       it 'returns 0 for new events' do
-        expect(event.vote_count).to eq(0)
+        expect(event.vote_count).to eq 0
       end
 
       it 'count the total number of votes' do
@@ -56,8 +61,8 @@ RSpec.describe Event do
       end
 
       it 'can create an array IDs of users who asked questions' do
-        event.questions.new(user_id: 1, copy: 'i am normal user?')
-        event.questions.new(user_id: 2, copy: 'i am admin user?')
+        event.questions.new(user_id: 1)
+        event.questions.new(user_id: 2)
 
         result = event.questioner_user_ids
         expect(result).to eq [1, 2]
