b7ba9f47d92a47e99fac643cf95dff0d6f918565
Mitchell Anicas <thisismitch@gmail.com>
2015-12-06 12:08:27 -0500

    update event sorting

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -91,7 +91,7 @@ class EventsController < ApplicationController
   end
 
   def set_events
-    @events = Event.all
+    @events = Event.order(:scheduled_at, created_at: :desc)
   end
 
   def set_users_questions_votes_count
