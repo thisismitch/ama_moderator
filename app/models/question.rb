0ac64d6ee57f893a3f0b783ce6d592418e51715a
Mitchell Anicas <thisismitch@gmail.com>
2018-07-29 18:22:09 -0400

    update my work

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,9 +3,10 @@ class Question < ActiveRecord::Base
   validates :copy, presence: true, length: { minimum: 3 }
 
   belongs_to :user
-  belongs_to :event
+  belongs_to :events
   has_many :votes, dependent: :destroy
   has_many :responses, dependent: :destroy
+  has_many :friends, dependent: :destroy
 
   scope :approved_anonymous, -> { where('anonymous_flag IS NULL OR anonymous_flag = false OR (anonymous_flag = true AND admin_approved_at IS NOT NULL)') }
 
